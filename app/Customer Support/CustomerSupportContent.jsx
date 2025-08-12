02d0d6afabc868fe362ed82de9bab3bffc31d23d
karopeter <karopeter531@gmail.com>
2025-08-12 05:51:18 -0700

    updates

---
+++ app/Customer Support/CustomerSupportContent.jsx
@@ -0,0 +1,7 @@
+
+
+export default function CustomerSupportContent() {
+   return (
+     <h1 className="text-center text-[#525252]">Coming Soon</h1>
+   );
+}
---
+++ app/CustomerReviews/CustomerReviewsContent.jsx
@@ -0,0 +1,7 @@
+
+
+export default function CustomerReviewsContent() {
+     return (
+      <h2 className="text-center text-[#525252]">Coming Soon</h2>
+     );
+}
